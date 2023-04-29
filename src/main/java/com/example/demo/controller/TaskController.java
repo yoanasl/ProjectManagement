500fbcd244ea9df2b17584fd32f831f0409e2132
Marina <marinazaykova200410@gmai.com>
2023-04-30 00:35:46 +0300

    created endpoints editForm and edit in TaskController;

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -24,6 +24,27 @@ public class TaskController {
         return "taskView";
     }
 
+    @GetMapping("/editForm/{id}")
+    public String showEditTaskForm(@PathVariable("id") Long id, Model model) {
+        // retrieve the task with the given id from the database
+
+        Task task = service.getTaskById(id);
+
+        // add the task to the model so it can be displayed in the edit form
+        model.addAttribute("task", task);
+
+        return "editTask"; // return the name of the Thymeleaf template for rendering the edit form
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateTask(@PathVariable("id") Long id, @ModelAttribute("task") Task task) {
+        // update the task in the database
+        task.setId(id); // set the id of the task to the id specified in the URL path
+       // taskRepository.save(task);
+
+        return "redirect:/task/get/" + id; // redirect to the task view page for the updated task
+    }
+
     //we are in the view of some particular project and we have button for create task
     @PostMapping("/create")
     public void createTask(@RequestBody CreateTaskRequest request) {
